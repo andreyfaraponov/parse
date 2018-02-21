d401f600942cd2b63ea9706ada98bc67cee75d38
Andrey FARAPONOV <afarapon@e1r5p2.unit.ua>
2018-02-21 13:24:02 +0200

    test1.1

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// Example express application adding the parse-server module to expose Parse
-// compatible API routes.
-
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+var ParseDashboard = require('parse-dashboard');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
@@ -11,16 +9,44 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
+var options = { allowInsecureHTTP: false };
+
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'test',
+  masterKey: process.env.MASTER_KEY || 'test2', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
 });
+
+var dashboard = new ParseDashboard({
+	"apps": [
+	  {
+		"serverURL": "mongodb://localhost:27017/dev",
+		"appId": "test",
+		"masterKey": "test2",
+		"appName": "Test_Server"
+	  }
+	],
+	"users": [
+		{
+			"user" : "xxxkappaxxx",
+			"pass" : "1234567890"
+		},
+		{
+			"user" : "ipostoen",
+			"pass" : "1234567890"
+		},
+		{
+			"user" : "admin",
+			"pass" : "1234567890"
+		}
+	],
+	"useEncryptedPasswords" : true
+  }, options);
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
@@ -29,6 +55,8 @@ var app = express();
 
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
+
+app.use('/dashboard', dashboard);
 
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
